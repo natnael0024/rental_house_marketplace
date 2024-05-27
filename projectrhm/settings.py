d03a62de05fc2dadaeeecc103ea164e762db5e7c
natnael0024 <nathanleg23@gmail.com>
2024-05-27 15:44:46 +0300

    host allowed

--- projectrhm/settings.py
+++ projectrhm/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','plenty-bikes-judge.loca.lt']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','plenty-bikes-judge.loca.lt','yba.onrender.com']
 
 
 # Application definition
