75274682b972b4ba6dbcd5021df4cb59358c07ef
natnael0024 <nathanleg23@gmail.com>
2024-05-28 15:04:32 +0300

    messaging added

--- projectrhm/urls.py
+++ projectrhm/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('listings/', include('listing.urls')),
+    path('messaging/', include('messaging.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
