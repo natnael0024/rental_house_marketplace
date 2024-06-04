c9506408752032500c5bc3298c3a631fd9716891
natnael0024 <nathanleg23@gmail.com>
2024-06-04 12:20:45 +0300

    ads form

--- admindashboard/urls.py
+++ admindashboard/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-
 urlpatterns = [
-  
-
-    path('',  views.dashboard,  name='dash' ),
-
-    
+    path('',  views.dashboard,  name='admin-dashboard' ),
+    path('users',  views.get_users,  name='admin-users' ),
+    path('users/<int:id>/action',  views.ban_unban_user,  name='admin-action'),
+    path('ads',  views.create_add,  name='admin-ads'),
+    path('ads/<int:id>',  views.set_ad_status,  name='admin-ads-status'),
 ]
