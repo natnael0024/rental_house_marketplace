284a5b463dd4c14d9ecdc267b14341a2849e3856
natnael0024 <nathanleg23@gmail.com>
2024-06-05 14:42:47 +0300

    bug fix

--- listing/models.py
+++ listing/models.py
@@ -14,6 +14,9 @@ class SubCity(models.Model):
     name = models.CharField(max_length=50)
     class Meta:
         db_table = 'subcities'
+    
+    def __str__(self):
+        return self.name
 
 class Listing(models.Model):
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name='listings')
